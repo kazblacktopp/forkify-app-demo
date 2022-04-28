85e40e5323afaee7ea7306308fa661ed5672ff19
kazblacktopp <karen.blacktopp@gmail.com>
2022-04-28 16:14:39 +1000

    Added search results view

---
+++ src/js/views/searchResultsView.js
@@ -0,0 +1,32 @@
+import View from './View.js';
+
+class SearchResultsView extends View {
+  _parentEl = document.querySelector('.results');
+
+  _generateMarkup() {
+    return this._data.map(this._generateSearchResults).join('');
+  }
+
+  _generateSearchResults(result) {
+    return `
+      <li class="preview--js">
+        <a href="#" class="preview__link">
+          <figure class="preview__fig">
+            <img
+              src="${result.image}"
+              class="preview__img"
+              alt="${result.title}"
+            />
+          </figure>
+
+          <div class="preview__data">
+            <h4 class="preview__title">${result.title}</h4>
+            <p class="preview__publisher">${result.publisher}</p>
+          </div>
+        </a>
+      </li>
+    `;
+  }
+}
+
+export default new SearchResultsView();
