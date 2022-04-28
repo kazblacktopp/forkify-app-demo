44591300f22b758688091aae8625764b2b3c96c3
kazblacktopp <karen.blacktopp@gmail.com>
2022-04-28 16:12:17 +1000

    Added handler function for search results view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,14 @@
 import { async } from 'regenerator-runtime';
 import * as model from './model.js';
 import searchView from './views/searchView.js';
+import searchResultsView from './views/searchResultsView.js';
 
 async function controlSearchResults() {
   try {
     const query = searchView.getQuery();
     if (!query) return;
     await model.loadSearchResults(query);
+    searchResultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
