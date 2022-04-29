3d09c01b617d21864161fc7c3fe8688788061ce4
kazblacktopp <karen.blacktopp@gmail.com>
2022-04-29 16:11:05 +1000

    Added handler function for pagination buttons

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,20 +2,28 @@ import { async } from 'regenerator-runtime';
 import * as model from './model.js';
 import searchView from './views/searchView.js';
 import searchResultsView from './views/searchResultsView.js';
+import paginationView from './views/paginationView.js';
 
 async function controlSearchResults() {
   try {
     const query = searchView.getQuery();
     if (!query) return;
     await model.loadSearchResults(query);
-    searchResultsView.render(model.state.search.results);
+    searchResultsView.render(model.getSearchResultsPage());
+    paginationView.render(model.state.search);
   } catch (err) {
     console.error(err);
   }
 }
 
+function controlPagination(goToPage) {
+  searchResultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+}
+
 function init() {
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerPagination(controlPagination);
 }
 
 init();
