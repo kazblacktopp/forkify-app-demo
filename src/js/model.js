e1a1810a68e6959c1237f7e7647824c5f2ace7b2
kazblacktopp <karen.blacktopp@gmail.com>
2022-04-29 16:11:51 +1000

    Added function to control search results

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,20 @@
 import { async } from 'regenerator-runtime';
 import { getJSON } from './helper.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 
 export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    numResultsPages: 1,
+    resultsPerPage: RESULTS_PER_PAGE,
   },
 };
 
 export async function loadSearchResults(query) {
   try {
-    const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${query}`;
+    const url = `${API_URL}?search=${query}`;
     const data = await getJSON(url);
     state.search.query = query;
 
@@ -22,9 +26,16 @@ export async function loadSearchResults(query) {
         publisher: rec.publisher,
       };
     });
-    console.log(state.search.results);
   } catch (err) {
     console.error(err);
     throw err;
   }
+}
+
+export function getSearchResultsPage(page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const finish = page * state.search.resultsPerPage;
+  const results = state.search.results.slice(start, finish);
+  return results;
 }
