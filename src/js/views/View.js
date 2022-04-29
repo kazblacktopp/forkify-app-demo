2f6b2ff6897e193e8b12767bc451d10f59e7f2b1
kazblacktopp <karen.blacktopp@gmail.com>
2022-04-29 16:13:34 +1000

    Added guard clause and function to clear markup

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,12 +2,21 @@ export default class View {
   _data;
 
   render(data) {
+    if (!data || (Array.isArray(data) && data.length === 0)) return;
+
     this._data = data;
+
     const markup = this._generateMarkup();
+
+    this._clearMarkup();
     this._insertMarkup(markup);
   }
 
   _insertMarkup(markup) {
     this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _clearMarkup() {
+    this._parentEl.innerHTML = '';
   }
 }
