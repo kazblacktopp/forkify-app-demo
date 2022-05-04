fea896b57e8af64c0d90add5420b8e31a97bdc10
kazblacktopp <karen.blacktopp@gmail.com>
2022-05-04 14:00:13 +1000

    Added controlRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,20 @@
 import { async } from 'regenerator-runtime';
 import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import searchResultsView from './views/searchResultsView.js';
 import paginationView from './views/paginationView.js';
+
+async function controlRecipe() {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    await model.loadRecipe(id);
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function controlSearchResults() {
   try {
@@ -22,6 +34,7 @@ function controlPagination(goToPage) {
 }
 
 function init() {
+  recipeView.addHandlerRender(controlRecipe);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerPagination(controlPagination);
 }
