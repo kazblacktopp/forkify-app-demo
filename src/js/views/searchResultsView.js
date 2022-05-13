1e291f9181eb3cd2b3ea5a51c47b19222e433bd9
kazblacktopp <karen.blacktopp@gmail.com>
2022-05-13 16:26:16 +1000

    Added errorMessage to view and called previewView to generate markup

--- src/js/views/searchResultsView.js
+++ src/js/views/searchResultsView.js
@@ -1,31 +1,12 @@
 import View from './View.js';
+import previewView from './previewView.js';
 
 class SearchResultsView extends View {
   _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query. Please try again!';
 
   _generateMarkup() {
-    return this._data.map(this._generateSearchResults).join('');
-  }
-
-  _generateSearchResults(result) {
-    return `
-      <li class="preview--js">
-        <a href="#${result.id}" class="preview__link">
-          <figure class="preview__fig">
-            <img
-              src="${result.image}"
-              class="preview__img"
-              alt="${result.title}"
-            />
-          </figure>
-
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>
-    `;
+    return this._data.map(previewView.generatePreview).join('');
   }
 }
 
