43cdcf9267d859c7811162f92da07a4c73719d3e
kazblacktopp <karen.blacktopp@gmail.com>
2022-05-13 16:26:53 +1000

    Added renderMessage and renderError functions to View

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,15 +1,35 @@
+import icons from 'url:../../img/icons.svg';
+
 export default class View {
   _data;
 
-  render(data) {
-    if (!data || (Array.isArray(data) && data.length === 0)) return;
-
-    this._data = data;
-
-    const markup = this._generateMarkup();
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
 
     this._clearMarkup();
     this._insertMarkup(markup);
+  }
+
+  render(data) {
+    try {
+      if (!data || (Array.isArray(data) && data.length === 0))
+        throw new Error(this._errorMessage);
+
+      this._data = data;
+
+      const markup = this._generateMarkup();
+
+      this._clearMarkup();
+      this._insertMarkup(markup);
+    } catch (err) {
+      throw err;
+    }
   }
 
   update(data) {
@@ -27,6 +47,42 @@ export default class View {
         curEl.innerHTML = newEl.innerHTML;
       }
     });
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg class="message__icon">
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p class="message__text">
+          ${message}
+        </p>
+      </div>
+    `;
+
+    this._clearMarkup();
+    this._insertMarkup(markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `
+      <div class="error">
+        <div>
+          <svg class="error__icon">
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p class="error__text">
+          ${message}
+        </p>
+      </div>
+    `;
+
+    this._clearMarkup();
+    this._insertMarkup(markup);
   }
 
   _insertMarkup(markup) {
